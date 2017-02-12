7429268a0b81c4da9636fd3080f527870786cc30
Acruz029 <andrewcruz1000@gmail.com>
2017-02-11 18:47:14 -0800

    Added page switch

--- button.js
+++ button.js
@@ -1,4 +1,13 @@
-function buttonclick () {
-document.getElementById("key").innerHTML = "Hello!";
+function sendToPage(){
+  var input = document.getElementById("search").value;
+  location.href = "mapSearch.html";
+}
 
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	   zoom: 10,
+	   center: new google.maps.LatLng(34.056528,-117.821529),
+     mapTypeId: 'terrain'
+   } )
 };
